4f300894e10f679cf30d3ed69125324b78b04510
Swapnil <swapforever>
2017-10-29 03:39:47 +0530

    Files Committed

---
+++ prog1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a=10,b=20,s=0;
+	s=a+b;
+	printf("%d",s);
+
+	return 0;
+
+
+
+}
---
+++ prog3.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+class prog3
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hellllooooaaaaaaaa Worldd");
+	}
+}
+
